23297e110183d6b8ba4c2c2a1b4f6441145cfd37
tainguyen211 <nguyennam210720011@gmail.com>
2025-09-05 11:18:02 +0700

    Fix ProjectsSection import in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Navigation from './components/Navigation';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
 import ExperienceSection from './components/ExperienceSection';
-import ProjectsSection from './components/ProjectsSection';
+import { ProjectsSection } from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import ScrollProgress from './components/ui/ScrollProgress';
